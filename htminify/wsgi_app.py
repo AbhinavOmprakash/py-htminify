98af28f0cc4c1d2ea66bb45d6553a896c77ae4eb
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-05-19 18:19:53 +0530

    formatting and doc strings

--- htminify/wsgi_app.py
+++ htminify/wsgi_app.py
@@ -1,11 +1,14 @@
-"""wsgi middleware """
 from htminify import minify
 
+
 class StripWhitespaceMiddleware(object):
-    def __init__(self, app):
+    """wsgi middleware that strips extra whitespace from html"""
+
+    def __init__(self, app, encoding="utf-8"):
         self.app = app
+        self.encoding = encoding
 
     def __call__(self, environ, start_response):
         app_iter = self.app(environ, start_response)
-        html = b"".join(app_iter).decode("utf8")
-        return [minify(html).encode("utf8")]
+        html = b"".join(app_iter).decode(self.encoding)
+        return [minify(html).encode(self.encoding)]
