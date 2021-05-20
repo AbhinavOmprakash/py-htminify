5943f2da77b34f70c7054e1ab52bbb1578dab08c
Abhinav Omprakash <55880260+AbhinavOmprakash@users.noreply.github.com>
2021-05-20 19:36:05 +0530

    typo

--- tests/helpers.py
+++ tests/helpers.py
@@ -5,7 +5,7 @@ class WsgiApp:
     def __init__(self, environ, start_response):
         self.environ = environ
         self.start_response = start_response
-        # convenience attribut for use in test
+        # convenience attributes for use in tests.
         self.original_html = "<html>           </html>".encode("utf-8")
         self.minified_html = ["<html></html>".encode("utf-8")]
 
