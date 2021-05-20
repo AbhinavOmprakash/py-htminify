f3540905db1d0905797c87c21a5f0181558dd901
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-05-20 16:50:16 +0530

    Added doc strings

--- htminify/wsgi.py
+++ htminify/wsgi.py
@@ -9,6 +9,7 @@ class StripWhitespaceMiddleware(object):
         self.encoding = encoding
 
     def __call__(self, environ, start_response):
+        """ Call during request/response cycle"""
         responseInterceptor = ResponseInterceptor(self.app, environ, start_response)
         html = responseInterceptor.get_response_content()
         modified_html = self._modify_response_content(html)
@@ -18,6 +19,7 @@ class StripWhitespaceMiddleware(object):
         return [modified_html]
 
     def _modify_response_content(self, content):
+        """Converts the content to a string using appropriate encoding."""
         content = b"".join(content)
         content = content.decode(self.encoding)
         modified_content = minify(content)
@@ -26,6 +28,11 @@ class StripWhitespaceMiddleware(object):
 
 # TODO find a better name for class. move to another file?
 class ResponseInterceptor:
+    """Intercepts the start_response function called by the app. 
+
+    This is necessary to modify the content headers when minifying the Content,
+    otherwise The webpage won't render."""
+
     def __init__(self, app, environ, start_response):
         self.app = app
         self.environ = environ
@@ -37,25 +44,31 @@ class ResponseInterceptor:
         self.modified_headers = []
 
     def get_response_content(self):
-        """Returns Iterable Byte string"""
+        """Returns an iterable app that can be iterated to get the response bytes.
+
+        A closure is necessary because when self.app() is called, It has the side effect
+        of calling start_response(status, headers, exc_info)
+        So the headers need to be captured so that they can be modified"""
+        
         return self.app(self.environ, self._capture_response_values)
 
     def _capture_response_values(self, status, headers, exc_info=None):
-        # Just collects the inner response headers,
-        # to be modified before sending to client
-        # Not calling start_response(), as we will modify the headers first.
+        """A closure that intercepts the headers sent by app."""
         self.captured_status = status
         self.captured_headers = headers
         self.captured_exc_info = exc_info
         return None
 
     def modify_content_headers(self, modified_content) -> None:
+        """ Modifies the content length header since minifying HTML reduces content length."""
         self.modified_headers = [
             (k, v) for k, v in self.captured_headers if k.lower() != "content-length"
         ]
         self.modified_headers.append(("Content-Length", str(len(modified_content))))
 
     def send_modified_response_headers(self):
+        """ Since the start_response() function was not triggered when self.app() was called,
+        it has to be manually triggered."""
         self.start_response(
             self.captured_status, self.modified_headers, self.captured_exc_info
         )
