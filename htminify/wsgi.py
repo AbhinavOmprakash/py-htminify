1dc7ae26441c8b764fd639494f828790a856488f
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-05-20 16:50:35 +0530

    Formatted with black

--- htminify/wsgi.py
+++ htminify/wsgi.py
@@ -9,7 +9,7 @@ class StripWhitespaceMiddleware(object):
         self.encoding = encoding
 
     def __call__(self, environ, start_response):
-        """ Call during request/response cycle"""
+        """Call during request/response cycle"""
         responseInterceptor = ResponseInterceptor(self.app, environ, start_response)
         html = responseInterceptor.get_response_content()
         modified_html = self._modify_response_content(html)
@@ -28,7 +28,7 @@ class StripWhitespaceMiddleware(object):
 
 # TODO find a better name for class. move to another file?
 class ResponseInterceptor:
-    """Intercepts the start_response function called by the app. 
+    """Intercepts the start_response function called by the app.
 
     This is necessary to modify the content headers when minifying the Content,
     otherwise The webpage won't render."""
@@ -49,7 +49,7 @@ class ResponseInterceptor:
         A closure is necessary because when self.app() is called, It has the side effect
         of calling start_response(status, headers, exc_info)
         So the headers need to be captured so that they can be modified"""
-        
+
         return self.app(self.environ, self._capture_response_values)
 
     def _capture_response_values(self, status, headers, exc_info=None):
@@ -60,14 +60,14 @@ class ResponseInterceptor:
         return None
 
     def modify_content_headers(self, modified_content) -> None:
-        """ Modifies the content length header since minifying HTML reduces content length."""
+        """Modifies the content length header since minifying HTML reduces content length."""
         self.modified_headers = [
             (k, v) for k, v in self.captured_headers if k.lower() != "content-length"
         ]
         self.modified_headers.append(("Content-Length", str(len(modified_content))))
 
     def send_modified_response_headers(self):
-        """ Since the start_response() function was not triggered when self.app() was called,
+        """Since the start_response() function was not triggered when self.app() was called,
         it has to be manually triggered."""
         self.start_response(
             self.captured_status, self.modified_headers, self.captured_exc_info
